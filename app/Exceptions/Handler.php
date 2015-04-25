49ecef3e7dea4e14470189f66f7d35e6a07b6ad2
Riari <hello@ricko.me>
2015-04-25 14:22:20 +0100

    Return 404 for ModelNotFoundException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,6 +36,10 @@ class Handler extends ExceptionHandler {
 	 */
 	public function render($request, Exception $e)
 	{
+		if ($e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
+		    return response()->view('errors.404', [], 404);
+		}
+
 		return parent::render($request, $e);
 	}
 
