90bc25b2c8f2cf45cd60834149027a252c399ed1
Riari <hello@ricko.me>
2017-04-11 10:07:39 +0100

    Start fresh with ponzu

---
+++ cmd/ponzu/vendor/github.com/ponzu-cms/ponzu/system/api/server.go
@@ -0,0 +1,16 @@
+package api
+
+import "net/http"
+
+// Run adds Handlers to default http listener for API
+func Run() {
+	http.HandleFunc("/api/contents", Record(CORS(Gzip(contentsHandler))))
+
+	http.HandleFunc("/api/content", Record(CORS(Gzip(contentHandler))))
+
+	http.HandleFunc("/api/content/create", Record(CORS(createContentHandler)))
+
+	http.HandleFunc("/api/content/update", Record(CORS(updateContentHandler)))
+
+	http.HandleFunc("/api/content/delete", Record(CORS(deleteContentHandler)))
+}
