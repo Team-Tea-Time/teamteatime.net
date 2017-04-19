3fd9a8a56195ee9e6e062b370b984facd0947fc5
Riari <hello@ricko.me>
2017-04-19 16:47:15 +0100

    Move to ExpressJS backend

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import http from 'http';
+import express from 'express';
+
+let app = express();
+app.server = http.createServer(app);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!!');
+})
+
+app.server.listen(process.env.PORT || '8080');
+
+console.log(`Started on port ${app.server.address().port}`);
+
+export default app;
