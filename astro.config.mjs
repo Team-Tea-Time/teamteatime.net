efd7c7c0a1ddc8bfe9e96d8cd575f7143cdd10b4
Rick Mann <3583774+Riari@users.noreply.github.com>
2024-04-07 11:30:40 +0100

    Remove redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/docs/laravel-forum": "/docs/laravel-forum/6.x/general",
-    "/docs/laravel-forum/[version]": "/docs/laravel-forum/[version]/general"
+    "/docs/laravel-forum": "/docs/laravel-forum/6.x/general"
   },
   integrations: [mdx()]
 });
